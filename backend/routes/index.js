548bde23fcd0d4aab56cf739b0c5530333e90e97
Ptungzz <phamtunglp2003@gmail.com>
2025-08-15 00:16:28 +0700

    Update add role permission for new role

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,6 +8,7 @@ const categoryRoutes = require("routes/categoryRoutes");
 const postRoutes = require("routes/postRoutes");
 const roleRoutes = require("routes/roleRoutes");
 const languageRoutes = require("routes/languageRoutes");
+const rolePermissionRoutes = require("routes/rolePermissionRoutes");
 // Add other routes as needed
 
 // Use module-specific routes
@@ -17,6 +18,7 @@ router.use("/categories", categoryRoutes);
 router.use("/posts", postRoutes);
 router.use("/role", roleRoutes);
 router.use("/languages", languageRoutes);
+router.use("/role-permissions", rolePermissionRoutes);
 // // Add other routes as needed
 
 module.exports = router;
